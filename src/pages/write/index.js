add039e9534cb7b4b550bdce438d7877c4cd5dc2
JadenPo <54308076+JadenPo@users.noreply.github.com>
2021-04-18 19:00:43 +0800

    add style.js for Write components

--- src/pages/write/index.js
+++ src/pages/write/index.js
@@ -1,13 +1,16 @@
 import React, { PureComponent } from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import { EssayWrapper } from "./style";
 
 class Write extends PureComponent {
     render() {
         const { loginStatus } = this.props;
 
         if (loginStatus) {
-            return <div>Write your own essay here.</div>;
+            return (
+                <EssayWrapper placeholder='Write your essay here!'></EssayWrapper>
+            );
         } else {
             return <Redirect to='/login' />;
         }
---
+++ src/pages/write/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const EssayWrapper = styled.textarea`
+    position: absolute;
+    display: block;
+    border: none;
+    top: 50px;
+    margin: 40px auto;
+    width: 500px;
+    height: 300px;
+`;
